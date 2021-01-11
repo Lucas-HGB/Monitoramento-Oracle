b42eefbf6c76ad03e9fea5fef08be0ef09ad8e87
Lucas-HGB <lucas.hgb2@gmail.com>
2021-01-11 11:29:57 -0300

    Python2 rework

--- pkg/pyOracle.py
+++ pkg/pyOracle.py
@@ -1,10 +1,11 @@
-from platform import system
-if system().lower() == "windows":
-	import winreg as reg
+#!/usr/bin/python2
+# -*- coding: UTF-8 -*-
+from platform import system as arch
 from timeit import timeit
 from argparse import ArgumentParser
 from Oracle_Scripts import choose_script
 from os import system
+from cx_Oracle import connect
 
 
 global start
@@ -28,36 +29,39 @@ def args():
                         help="Debug", metavar="debug", default=False)
     args = parser.parse_args()
     return args
-	
+
 def print_json(values):
-	print("{")
-	print('    "data": [')
+	print "{"
+	print '    "data": ['
 	for varia in values:
-		print('        {')
+		print '        {'
 		if varia != values[-1]:
-			print('            "{#SID}": "%s"},' % varia)
+			print '            "{#SID}": "%s"},' % varia
 		else:
-			print('            "{#SID}": "%s"}' % varia)
-	print('    ]')
-	print("}")
+			print '            "{#SID}": "%s"}' % varia
+	print '    ]'
+	print "}"
 
 class Banco():
 
 	def __init__(self):
 		pass
 
-	#Conexão com banco de dados
-	def connect(self, ip, user, password, port = 1521,instance = False, Banco = False):
-		try:
+	def connect(self, ip, user, password, port = 1521,instance = False):
 			from cx_Oracle import connect
-			self.connection = connect(user,password,f"{ip}:{port}/{instance}",encoding="UTF-8")
-			self.cursor = self.connection.cursor()
-		except Exception as e:
-			print(f"Error {e} occurred!")
+			try:
+				self.connection = connect(user, password, "%s:%s/%s" % (ip, port, instance), encoding="UTF-8")
+				self.cursor = self.connection.cursor()
+			except Exception as excp:
+				print excp
 
 	def run_command(self, command, value):
-		output = choose_script(cursor = self.cursor, opc = command, value = value)
-		return output
+		try:
+			output = choose_script(cursor = self.cursor, opc = command, value = value)
+			return output
+		except AttributeError:
+			pass
+		
 
 class Ambiente():
 
@@ -74,7 +78,7 @@ class Ambiente():
 	def get_oracle_configs(self):
 		global data
 		configs = {}
-		if system().lower() == "linux":
+		if arch().lower() == "linux":
 			try:
 				with open(r"/etc/oratab", "r") as oratab:
 					lines = oratab.readlines()
@@ -82,8 +86,8 @@ class Ambiente():
 					configs[config_line.split(":")[0]] = (config_line.split(":")[1])
 				return configs
 			except FileNotFoundError:
-				print("File /etc/oratab not found!")
-		elif system().lower() == "windows":
+				print "File /etc/oratab not found!"
+		elif arch().lower() == "windows":
 			reg_path = r'SOFTWARE\Oracle\KEY_HOME_NAME'
 			reg_key = reg.OpenKey(reg.HKEY_LOCAL_MACHINE, reg_path, 0, reg.KEY_READ)
 			data = reg.QueryValue(reg_key, None)
@@ -97,26 +101,26 @@ if __name__ == "__main__":
 	if args.debug:
 		args.debug = True
 	if args.debug:
-		print(f"Extraindo argumentos do CMD {get_time()}")
+		print "Extraindo argumentos do CMD %s" % get_time()
 	if args.command != "ora_configs" and args.command != "pyversion" and args.command != "home":
 		Banco.connect(ip = "localhost",user = args.user, password = args.password, instance = args.instance)
 		if args.debug:
-			print(f"Conectado {get_time()}")
+			print "Conectado %s " % get_time()
 		if args.debug:
-			print(f"Rodando comando {get_time()}")
+			print "Rodando comando %s" % get_time()
 		if args.value:
 			Banco.run_command(command = args.command, value = args.value)
 		elif not args.value:
 			Banco.run_command(command = args.command, value = None)
 		if args.debug:
-			print(f"Comando finalizado {get_time()}")		
+			print "Comando finalizado %s" % get_time()		
 	elif args.command == "home":
 		if args.debug:
-			print(f"Extraindo home de SID específica {get_time()}")
+			print "Extraindo home de SID específica %s" % get_time()
 		ora_configs = Ambiente.get_oracle_configs()
 		print(ora_configs[args["-s"]])
 	elif args.command == "pyversion":
-		print(f"pyOracle Version {Ambiente.get_pyora_version()}")
+		print "pyOracle Version %s" % Ambiente.get_pyora_version()
 	elif args.command == "ora_configs":
 		ora_configs = Ambiente.get_oracle_configs()
 		try:
