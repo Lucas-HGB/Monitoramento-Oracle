cf09871c7b375e05d97597e5d49d23bdd2631b3b
Lucas-HGB <69653148+Lucas-HGB@users.noreply.github.com>
2021-01-12 10:25:32 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,18 +14,19 @@ def get_os_ver():
     return ver
 
 def pyOracle_setup():
+    system("sudo yum install oracle-epel-release-el7.x86_64"
     system("sudo yum install -y python2-pip")
     system("python2 -m pip install cx_Oracle==7.3")
 
 def install_zabbix():
     ver = get_os_ver()
     if ver >= 7:
-        system("rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm")
+        system("rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm")
     elif ver >= 6:
-        system("rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/6/x86_64/zabbix-release-4.0-1.el6.noarch.rpm")
+        system("rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/6/x86_64/zabbix-release-4.4-1.el6.noarch.rpm")
     elif ver >= 5:
-        system("rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/5/x86_64/zabbix-release-4.0-1.noarch.rpm")
-    system("sudo yum install -y zabbix-agent zabbix-sender")
+        system("rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/5/x86_64/zabbix-release-4.4-1.el5.noarch.rpm")
+    system("sudo yum install -y zabbix-agent-4.4.6 zabbix-get-4.4.6 zabbix-sender-4.4.6")
     system("rm -rf /etc/zabbix")
 
 
